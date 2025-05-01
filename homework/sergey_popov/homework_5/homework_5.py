30ba718314dc46c64d40f835f995825e18f09964
Sergey Popov <ser78910111@gmail.com>
2025-05-02 00:38:07 +0500

    sergey_popov_homework_5

---
+++ homework/sergey_popov/homework_5/homework_5.py
@@ -0,0 +1,27 @@
+# lesson_1
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+name, last_name, city, phone, country = person
+print(name, last_name, city, phone, country)
+
+# lesson_2
+result_1 = 'результат операции: 42'
+one_num = int(result_1[19:])
+print(one_num + 10)
+
+result_2 = 'результат операции: 514'
+two_num = int(result_2[19:])
+print(two_num + 10)
+
+result_3 = 'результат работы программы: 9'
+three_num = int(result_3[27:])
+print(three_num + 10)
+
+# lesson_3
+students = ['Ivanov', 'Petrov', 'Sidorov']
+students = ', '.join(students)
+subjects = ['math', 'biology', 'geography']
+subjects = ', '.join(subjects)
+print('Students', students, 'study these subjects:', subjects)
+
+
+
