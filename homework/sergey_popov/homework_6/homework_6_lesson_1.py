8f90067b25ff5e70f4b5b06d3e1b87076682f88f
Sergey Popov <ser78910111@gmail.com>
2025-05-03 01:11:41 +0500

    sergey_popov_homework_6

---
+++ homework/sergey_popov/homework_6/homework_6_lesson_1.py
@@ -0,0 +1,16 @@
+text = (
+    'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. ' 
+    'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+)
+
+words = text.split()
+
+for word in words:
+    if ',' in word:
+        word = word.replace(',', 'ing,')
+    if '.' in word:
+        word = word.replace('.', 'ing.')
+    elif ',' not in word:
+        word = word + 'ing'
+
+    print(word, end=' ')
