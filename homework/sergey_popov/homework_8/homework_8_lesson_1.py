929de761fed82dbeb69cf02cde591948a16cf298
Sergey Popov <ser78910111@gmail.com>
2025-05-08 17:34:51 +0500

    sergey_popov_homework_8

--- homework/sergey_popov/homework_8/homework_8_lesson_1.py
+++ homework/sergey_popov/homework_8/homework_8_lesson_1.py
@@ -3,7 +3,7 @@ import random
 salary = int(input())
 bonus = random.choice([True, False])
 
-if bonus == True:
+if bonus is True:
     sale = salary + random.randrange(1, 5000)
     print(f"{salary}, {bonus} - '${sale}'")
 else:
