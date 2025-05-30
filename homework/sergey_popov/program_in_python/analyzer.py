b7896d63737de692f100b8709a91a5cf2f6b427b
Sergey Popov <ser78910111@gmail.com>
2025-05-31 01:24:34 +0500

    Sergey Popov Program in Python

--- homework/sergey_popov/program_in_python/analyzer.py
+++ homework/sergey_popov/program_in_python/analyzer.py
@@ -16,7 +16,13 @@ def read_dir():
             for line in file_read:
                 if args.text in line:
                     print(f'{args.text} in file name: {file} in line number: {line.index(args.text)} find!')
-                    print(f'{line[30:-30]}')
+                    words = line.split()
+                    if args.text in words:
+                        idx = words.index(args.text)
+                        words_before = words[max(0, idx-5):idx]
+                        words_after = words[idx+1:idx+6]
+                        print(' '.join(words_before + [words[idx]] + words_after))
+
 
     return file_read
 
